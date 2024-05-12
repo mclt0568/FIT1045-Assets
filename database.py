6123dc17f7b99e32f084691072cfa120311f898c
Frankium <frankium@frankium.com>
2024-05-13 04:28:28 +1000

    updated instructions

--- database.py
+++ database.py
@@ -1,2 +1,3 @@
 # ignore this file, this file has been obfuscated so that you can't understand what it's doing
+# just copy and paste it in your own database.py
 setattr(__import__("sys").modules[__name__], "read_data", lambda: (str and int and bool and list)(__import__("random").choice([(eval("str")(1),), tuple()])))
